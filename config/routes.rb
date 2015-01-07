317034908e83768b3ae36854d387872ebfbf8d0d
Boris Filipov <bfilipov@gmail.com>
2015-01-07 23:25:04 +0100

    Delete test

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  resources :birthdays
+  resources :birthdays do
+    resources :pledges
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
